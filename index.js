d79efe046912b883e933c2fea2ec6b971cb15060
Malick <malick.s.mohamed@gamil.com>
2020-10-06 16:24:27 +0300

    Add request routing

--- index.js
+++ index.js
@@ -25,9 +25,44 @@ const server = (req, res) => {
     req.on('end', () => {
         buffer += decoder.end();
         
-        res.end('working ' + method + ' ' + trimmedPath);
+        const requestHandler = typeof router[trimmedPath] == 'undefined' ? handlers.notFound : router[trimmedPath];
+
+        const data = {
+            path: trimmedPath,
+            queries,
+            method,
+            payload: buffer
+        }
+        
+        requestHandler(data, (statusCode, payload) =>{
+            // Default status code 
+            statusCode = typeof statusCode == 'number' ? statusCode : 200;
+
+            // Default payload
+            payload = typeof payload == 'object' ? payload : {};
+
+            const paylaodAsString = JSON.stringify(payload)
+
+            res.setHeader('Content-Type', 'Application/json');
+            res.writeHead(statusCode);
+            res.end(paylaodAsString);
+        });
     });
 
 }    
 
 httpServer.listen(8080, () => console.log('server working on localhost:3030'))
+
+const handlers = {};
+
+handlers.test = (data, callback) => {
+   callback(200, {msg: "Testign the Routing thing"}); 
+}
+
+handlers.notFound = (data, callback) => {
+   callback(404) 
+}
+
+const router = {
+    'test': handlers.test
+}
